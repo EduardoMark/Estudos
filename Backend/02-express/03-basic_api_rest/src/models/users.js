6db4cfe9a53e525d10e01793082e6152c59604d3
Eduardo Marques <eduardomar1703@gmail.com>
2024-11-06 10:34:51 -0300

    aprendendo sobre API's REST

---
+++ Backend/02-express/03-basic_api_rest/src/models/users.js
@@ -0,0 +1,6 @@
+const users = [
+    {id: 1, name: "Eduardo", age: 21},
+    {id: 2, name: "John Doe", age: 44}
+];
+
+module.exports = users;
---
+++ Backend/02-express/03-basic_api_rest/src/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const userController = require('./controllers/userController');
+
+const router = express.Router();
+
+router.get('/', userController.index);
+router.get('/users/:id', userController.show);
+
+router.post('/users', userController.save);
+
+router.put('/users/:id', userController.update);
+
+router.delete('/users/:id', userController.delete);
+
+module.exports = router;

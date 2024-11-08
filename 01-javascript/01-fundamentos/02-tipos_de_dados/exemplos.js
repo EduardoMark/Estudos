652a0bc764d2f4018adc2da883a73d6aa2b821ce
Eduardo Marques <eduardomar1703@gmail.com>
2024-11-08 15:42:30 -0300

    Adiciona estudos sobre tipos de dados

---
+++ 01-javascript/01-fundamentos/02-tipos_de_dados/exemplos.js
@@ -0,0 +1,20 @@
+// numbers
+let height = 1.85;
+let length = 7;
+
+// strings
+let song = "Camaro Amarelo";
+let player = 'Jordan';
+
+// booleans
+let a = true;
+let z = false;
+
+// undefined;
+let num; // algo indefinido
+
+// null
+let umpty = null; // definido como vazio
+
+// bigint
+let bigger = 9999999999n;
